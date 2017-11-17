97228999fc6ea809618dced982686e1b40fac69e
Fiqih Nur Ramadhan <fiqih.nurramadhan@bukalapak.com>
2017-11-17 17:40:43 +0700

    retrieve Gift using price

--- Controllers/giftController.js
+++ Controllers/giftController.js
@@ -4,14 +4,35 @@ var service = mysql.createConnection(setupConnection.setCon);
 
 var retrieveGifts = function(req,res){
 	console.log("Starting retrieve all data");
-	var sql = "SELECT * FROM Gift";
-	service.query(sql, function (err, result, field) {
+	var priceRangeChecking
+	if (req.params.price < 100000) {
+		priceRangeChecking = 1
+	}
+	else if (priceRangeChecking > 100000 && priceRangeChecking < 500000) {
+		priceRangeChecking = 2
+	}
+	else {
+		priceRangeChecking = 3
+	}
+
+	var sql = "SELECT * FROM Gift Where price_range = ?";
+	service.query(sql, [priceRangeChecking], function (err, result, field) {
 	    if (err) {
 	    	res.json({status:400,message:'Failed'});
 	    } else {
 	    	res.json({status:200,message:'Get data success',data:result});
 	    }
 	});	
+
+	// service.on('error', function(err) {
+ //    console.log('db error', err);
+ //    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+ //      retrieveGifts(req,res);                         // lost due to either server restart, or a
+ //    } else {                                      // connnection idle timeout (the wait_timeout
+ //      throw err;                                  // server variable configures this)
+ //    }
+ //  });
+
 };
 
 
--- Routes/giftRouter.js
+++ Routes/giftRouter.js
@@ -3,7 +3,7 @@ var giftController=require('./../Controllers/giftController');
  
 var giftRouter=express.Router();
 
-giftRouter.route('/retrieveGifts')
+giftRouter.route('/retrieveGifts/:price')
 	.get(giftController.retrieveGifts);
 
 module.exports=giftRouter
