64981212a01edd20a5394591b4b1283228047fbf
Ryan Baskara <ryan.baskara@bukalapak.com>
2017-11-17 14:34:14 +0700

    wip check voucher

--- Controllers/inventoryController.js
+++ Controllers/inventoryController.js
@@ -5,11 +5,56 @@ var service = mysql.createConnection(setupConnection.setCon);
 var retrieveInventories = function(req,res){
 	service.connect(function(err) {
 	  if (err) throw err;
-	  console.log("Starting retrieve inventory with userId: "+ req.params.id );
-	  res.json({status:200,message:'Success',data:[]});
-		service.release()
+	  var userId = req.params.id
+	  console.log("Starting retrieve inventory with userId: "+ userId);
+	  var creds = [userId]
+	  var sql = "SELECT * FROM Gift_Inventory WHERE user_id = ?";
+	  service.query(sql, creds, function (err, result, field) {
+	    if (err) {
+	    	res.json({status:400,message:'Failed'});
+	    } else {
+	    	res.json({status:200,message:'Get data success',data:result});
+	    }
+	  	service.release()
+	  });
 	});	
 
+};
+
+var checkVoucher = function(req,res){
+  var userId = req.body.id
+  var voucherCode = req.body.voucher
+
+  var creds = [userId, voucherCode]
+  var sql = "SELECT * FROM Gift g, Gift_Inventory i WHERE g.id = i.gift_id AND i.user_id = ? AND g.voucher_code = ?";
+  service.query(sql, creds, function (err, result, field) {
+    if (err) {
+    	res.json({status:400,message:'Failed'});
+    } else {
+    	if (result.count() == 0) {
+    		res.json({status:200, message:'Voucher not falid', data:result});
+    	} else {
+    		res.json({status:200, message:'Get data success', data:result});
+    	}
+    }
+  	service.release()
+  });
+};
+
+var useInventories = function(req,res){
+  var userId = req.body.id
+  var voucherCode = req.body.voucher
+
+  var creds = [userId]
+  var sql = "SELECT * FROM Gift_Inventory WHERE user_id = ?";
+  service.query(sql, creds, function (err, result, field) {
+    if (err) {
+    	res.json({status:400,message:'Failed'});
+    } else {
+    	res.json({status:200,message:'Get data success',data:result});
+    }
+  	service.release()
+  });
 };
 
 module.exports = {
--- Routes/inventoryRouter.js
+++ Routes/inventoryRouter.js
@@ -5,5 +5,8 @@ var router = express.Router()
 
 router.route('/retrieveInventories/:id')
 	.get(controller.retrieveInventories)
-
+router.route('/checkVoucher')
+	.post(controller.checkVoucher)
+router.route('/useInventory')
+	.post(controller.useInventory)
 module.exports = router
